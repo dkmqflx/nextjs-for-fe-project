a30f994a40e75c412fd9dc77521e973ae54b205f
dkmqflx <dkmqflx@gmail.com>
2026-02-26 16:15:08 +0900

    51. [런칭준비] 프로덕션 DB 접속 및 마이그레이션

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,5 +1,10 @@
 import { registerAs } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  path: `env.${process.env.NODE_ENV || 'local'}`,
+});
 
 const config = {
   type: 'postgres',
@@ -13,6 +18,17 @@ const config = {
   autoLoadEntities: true,
   synchronize: false,
 };
+
+if (process.env.NODE_ENV === 'production') {
+  // @ts-expect-error - TypeORM의 ssl 옵션은 기본적으로 정의되어 있지 않습니다.
+  config.ssl = true;
+  // @ts-expect-error - TypeORM의 extra 옵션은 기본적으로 정의되어 있지 않습니다.
+  config.extra = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
 
 export default registerAs('typeorm', () => config);
 export const connectionSource = new DataSource(config as DataSourceOptions);
